661f2d4b62beec8ff9d424c9366d2dbd57272f34
Sahildhillon0 <sahildhillon609@gmail.com>
2024-10-06 15:50:32 +0530

    deploying on localchain

--- deploy/01_deploy-fund-me.js
+++ deploy/01_deploy-fund-me.js
@@ -1,5 +1,6 @@
 const { network } = require("hardhat");
 const { networkConfig } = require("../helper-hardhat-config");
+const { deploymentchains } = require("../helper-hardhat-config");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     // console.log("Deploying FundMe contract...");
@@ -7,11 +8,22 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deployer } = await getNamedAccounts();
     const chainId = network.config.chainId;
 
-    const ethUSDPriceFeedAddress = networkConfig[chainId]["ethUSDPriceFeed"];
+    // const ethUSDPriceFeedAddress = networkConfig[chainId]["ethUSDPriceFeed"];
+    let ethUSDPriceFeedAddress;
 
-    const fundMe = await deploy("fundMe", {
+    if (deploymentchains.includes(network.name)) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator");
+        ethUSDPriceFeedAddress = ethUsdAggregator.address;
+    } else {
+        ethUSDPriceFeedAddress = networkConfig[chainId]["ethUSDPriceFeed"];
+    }
+
+    const fundMe = await deploy("FundMe", {
         from: deployer,
         args: [ethUSDPriceFeedAddress],
         log: true,
     });
+    log("---------------------------------");
 };
+
+module.exports.tags = ["all", "FundMe"];
