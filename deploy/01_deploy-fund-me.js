e070dc4ca77fab6c328372a2360110c85648c13c
Sahildhillon0 <sahildhillon609@gmail.com>
2024-10-25 23:47:55 +0530

    Deplaying contract FundMe to Ethereum network

--- deploy/01_deploy-fund-me.js
+++ deploy/01_deploy-fund-me.js
@@ -1,6 +1,7 @@
 const { network } = require("hardhat");
 const { networkConfig } = require("../helper-hardhat-config");
 const { deploymentchains } = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     // console.log("Deploying FundMe contract...");
@@ -10,19 +11,29 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
     // const ethUSDPriceFeedAddress = networkConfig[chainId]["ethUSDPriceFeed"];
     let ethUSDPriceFeedAddress;
+    console.log("chainId", chainId);
 
     if (deploymentchains.includes(network.name)) {
         const ethUsdAggregator = await deployments.get("MockV3Aggregator");
         ethUSDPriceFeedAddress = ethUsdAggregator.address;
     } else {
-        ethUSDPriceFeedAddress = networkConfig[chainId]["ethUSDPriceFeed"];
+        ethUSDPriceFeedAddress = networkConfig[11155111]["ethUSDPriceFeed"];
     }
 
+    // const args = ethUSDPriceFeedAddress;
     const fundMe = await deploy("FundMe", {
         from: deployer,
         args: [ethUSDPriceFeedAddress],
         log: true,
+        waitConfirmations: network.config.blockconfirmations || 1,
     });
+
+    if (
+        !deploymentchains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, [ethUSDPriceFeedAddress], network.name);
+    }
     log("---------------------------------");
 };
 
